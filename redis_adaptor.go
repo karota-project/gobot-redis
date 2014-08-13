df4b21f5448a32cfe17e3ea9a011ea404826ff72
yuhki50 <yuhki50@gmail.com>
2014-08-13 13:27:22 +0900

    機能を実装

--- redis_adaptor.go
+++ redis_adaptor.go
@@ -1,26 +1,51 @@
 package redis
 
 import (
+	"github.com/garyburd/redigo/redis"
 	"github.com/hybridgroup/gobot"
 )
 
 type RedisAdaptor struct {
 	gobot.Adaptor
+	redisConn redis.Conn
 }
 
-func NewRedisAdaptor(name string) *RedisAdaptor {
+func NewRedisAdaptor(name string, port string) *RedisAdaptor {
 	return &RedisAdaptor{
 		Adaptor: *gobot.NewAdaptor(
 			name,
 			"redis.RedisAdaptor",
+			port,
 		),
 	}
 }
 
 func (r *RedisAdaptor) Connect() bool {
+	if r.Connected() == true {
+		disconnect(r)
+	}
+	connect(r)
 	return true
 }
 
 func (r *RedisAdaptor) Finalize() bool {
+	disconnect(r)
 	return true
+}
+
+func connect(r *RedisAdaptor) {
+	conn, err := redis.Dial("tcp", r.Port())
+	if err != nil {
+		panic(err)
+	}
+
+	r.redisConn = conn
+	r.SetConnected(true)
+}
+
+func disconnect(r *RedisAdaptor) {
+	if r.redisConn != nil {
+		r.redisConn.Close()
+	}
+	r.SetConnected(false)
 }
--- redis_driver.go
+++ redis_driver.go
@@ -1,6 +1,8 @@
 package redis
 
 import (
+	"fmt"
+	"github.com/garyburd/redigo/redis"
 	"github.com/hybridgroup/gobot"
 )
 
@@ -12,18 +14,49 @@ type RedisInterface interface {
 }
 
 func NewRedisDriver(a *RedisAdaptor, name string) *RedisDriver {
-	return &RedisDriver{
+	r := &RedisDriver{
 		Driver: *gobot.NewDriver(
 			name,
 			"redis.RedisDriver",
 			a,
 		),
 	}
+
+	r.AddCommand("Get", func(params map[string]interface{}) interface{} {
+		key := params["key"].(string)
+
+		value, err := redis.String(r.adaptor().redisConn.Do("GET", key))
+		if err == nil {
+			fmt.Println(value)
+		}
+
+		return true
+	})
+
+	r.AddCommand("Set", func(params map[string]interface{}) interface{} {
+		key := params["key"].(string)
+		value := params["value"].(string)
+
+		r.adaptor().redisConn.Do("SET", key, value)
+
+		return true
+	})
+
+	return r
 }
 
 func (r *RedisDriver) adaptor() *RedisAdaptor {
 	return r.Driver.Adaptor().(*RedisAdaptor)
 }
 
-func (r *RedisDriver) Start() bool { return true }
-func (r *RedisDriver) Halt() bool  { return true }
+func (r *RedisDriver) Start() bool {
+	return true
+}
+
+func (r *RedisDriver) Halt() bool {
+	return true
+}
+
+func (r *RedisDriver) RedisConn() redis.Conn {
+	return r.adaptor().redisConn
+}
